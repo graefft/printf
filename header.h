664068cce5f62f021412767123490a719d23ec95
scurry222 <scurry222@gmail.com>
2019-03-16 18:09:09 +0000

    first commit - working decimal string and char

---
+++ header.h
@@ -0,0 +1,16 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+typedef struct slist
+{
+        ;
+} format;
+
+int _printf(const char *fmt, ...);
+
+#endif
---
+++ simple_printf.c
@@ -0,0 +1,67 @@
+#include "header.h"
+
+/**
+*
+*
+*
+*
+*/
+
+int _printf(const char *fmt, ...)
+{
+	int i = 0, j = 0;
+	va_list args;
+	int dval;
+	int exp;
+	int temp;
+	char *sval;
+
+	va_start(args, fmt);
+
+        if (fmt == NULL)
+                return(1);
+
+        for(i = 0; fmt[i] != '\0'; i++)
+        {
+                if (fmt[i] == '%')
+                {
+                        i++;
+                        switch(fmt[i])
+                        {
+			case'd': case 'i':
+                                dval = va_arg(args, int);
+            	                if (dval == 0)
+                                        putchar('0');
+                                if (dval < 0)
+                                {
+                                        putchar ('-');
+                                        dval = dval * -1;
+                                }
+                                for (exp = 1; dval/exp != 0; exp = exp * 10)
+                                                ;
+                                while (dval != 0)
+                                {
+                                        exp = exp / 10;
+                                        temp = dval/exp;
+                                        putchar(temp + '0');
+                                        dval = dval % exp;
+                                }
+                                break;
+                        case 's':
+                                sval = va_arg(args, char *);
+                                for(j = 0; sval[j] != '\0'; j++)
+                                {
+                                        putchar(sval[j]);
+                                }
+                                break;
+                        case'c':
+                                dval = va_arg(args, int);
+                                putchar(dval);
+                                break;
+			}
+		}
+		else
+			putchar(fmt[i]);
+	}
+	return (0);
+}
