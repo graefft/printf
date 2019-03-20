b2a4c1c807008a06456987620241dd94eaa3e84e
scurry222 <scurry222@gmail.com>
2019-03-20 00:07:16 +0000

    adding address printer

--- helper_funcs_4.c
+++ helper_funcs_4.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+
 /**
 * print_string - print all chars in a string
 * @sval: passed in string
@@ -46,4 +47,25 @@ int print_perc(va_list c)
 
 	_putchar('%');
 	return (1);
+}
+
+/**
+* print_p - print address of a variable
+* @sval: passed in address (string)
+*
+* Return: count
+*/
+
+int print_p(va_list sval)
+{
+	unsigned long int p = va_arg(sval, unsigned long int);
+	int count = 0;
+	char *a;
+
+	a = convert(p, 16, 1);
+	count += _putchar('0');
+	count += _putchar('x');
+	count += _puts(a);
+
+	return (count);
 }
